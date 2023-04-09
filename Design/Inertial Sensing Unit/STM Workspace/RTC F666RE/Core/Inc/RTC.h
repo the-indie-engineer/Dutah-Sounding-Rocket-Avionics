564facabd8ae5cefd49190b34bb250fa59c54640
Thiruvarulselvan Karunanithi <109530150+the-indie-engineer@users.noreply.github.com>
2023-04-09 16:32:40 +0530

    Update RTC Firmware with time input feature

---
+++ Design/Inertial Sensing Unit/STM Workspace/RTC F666RE/Core/Inc/RTC.h
@@ -0,0 +1,29 @@
+/*
+ * RTC.h
+ *
+ *  Created on: 09-Apr-2023
+ *      Author: Thiruvarulselvan K
+ */
+
+#ifndef INC_RTC_H_
+#define INC_RTC_H_
+
+#include "main.h"
+
+#define DS3231_ADDRESS 0xD0
+
+typedef struct {
+	uint8_t seconds;
+	uint8_t minutes;
+	uint8_t hour;
+	uint8_t dayofweek;
+	uint8_t dayofmonth;
+	uint8_t month;
+	uint8_t year;
+} TIME;
+
+uint8_t decToBcd(int val);
+int bcdToDec(uint8_t val);
+
+
+#endif /* INC_RTC_H_ */
